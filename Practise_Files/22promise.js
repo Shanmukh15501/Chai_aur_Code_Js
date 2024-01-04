84652fbb67fa94f2b7ded86eaafc9b58e1ecf255
shanu <shanmukhadari@gmail.com>
2024-01-04 23:48:45 +0530

    removed space

--- Practise_Files/22promise.js
+++ Practise_Files/22promise.js
@@ -1,2 +1,3 @@
-//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value.
+//The Promise object represents the eventual completion (or failure) of an asynchronous operation and its resulting value
 
+  
